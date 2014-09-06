f7e57bfee0e90d8c9da83036a2d6fa8f8946ab34
Sylvain Estevez <estevez.sylvain@gmail.com>
2014-09-06 09:11:49 +0200

    WIP

--- src/logger/index.js
+++ src/logger/index.js
@@ -6,16 +6,6 @@ var _ = require('lodash');
 var util = require('util');
 var NOOT = require('../../')('object');
 
-/**
- * Default values for styling
- */
-var DEFAULT_STYLES = {
-  ALWAYS: { color: 'cyan', bold: true, underline: true },
-  ERROR: 'red',
-  WARN: 'yellow',
-  INFO: 'magenta',
-  TRACE: 'grey'
-};
 
 /***********************************************************************************************************************
  * Logger Class
@@ -30,12 +20,14 @@ var DEFAULT_STYLES = {
 var Logger = NOOT.Object.extend({
   level: null,
 
+  _styles: null,
+
   /**
    * Constructor
    */
   init: function() {
     this.setLevel(this.level);
-    this.styles = _.extend(_.cloneDeep(DEFAULT_STYLES), this.styles);
+    this.setStyles();
   },
 
   /**
@@ -43,7 +35,7 @@ var Logger = NOOT.Object.extend({
    */
   trace: function() {
     if (this.level <= Logger.levels.TRACE) {
-      this.writeLog(Logger._joinMessages(arguments), Logger.levels.TRACE);
+      return this._buildAndSend(arguments, 'trace');
     }
   },
 
@@ -52,7 +44,7 @@ var Logger = NOOT.Object.extend({
    */
   debug: function() {
     if (this.level <= Logger.levels.DEBUG) {
-      this.writeLog(Logger._joinMessages(arguments), Logger.levels.DEBUG);
+      return this._buildAndSend(arguments, 'debug');
     }
   },
 
@@ -61,7 +53,7 @@ var Logger = NOOT.Object.extend({
    */
   info: function() {
     if (this.level <= Logger.levels.INFO) {
-      this.writeLog(Logger._joinMessages(arguments), Logger.levels.INFO);
+      return this._buildAndSend(arguments, 'info');
     }
   },
 
@@ -70,63 +62,34 @@ var Logger = NOOT.Object.extend({
    */
   warn: function() {
     if (this.level <= Logger.levels.WARN) {
-      this.writeLog(Logger._joinMessages(arguments), Logger.levels.WARN);
+      return this._buildAndSend(arguments, 'warn');
+
     }
   },
 
   /**
    * Error level log
-   *
-   * @info if first arg is an Error, stack trace will be logged, else messages will be concatenated
    */
   error: function() {
-    if (this.level > Logger.levels.ERROR) return;
-
-    var message = arguments[0] instanceof Error ?
-                  arguments[0].stack.toString() :
-                  Logger._joinMessages(arguments);
-
-    this.writeLog(message, Logger.levels.ERROR);
-  },
-
-  /**
-   * Always print a title message without carring logging level
-   *
-   * @info mainly useful for application logs, i.e. server starting log
-   */
-  title: function() {
-    this.writeLog(Logger._joinMessages(arguments), Logger.levels.ALWAYS);
-  },
-
-  /**
-   * Write method / default is console
-   *
-   * @info override me to send your logs elsewhere, i.e. to a file
-   *
-   * @param message
-   * @param level
-   */
-  writeLog: function(message, level) {
-    var date = new Date().toISOString();
-    var color;
-    var isBold = false;
-    var underline = false;
-
-    level = _.findKey(Logger.levels, function(val) { return level === val; });
-
-    color = (level ? this.styles[level] : '') || 'white';
-
-    if (color instanceof Object) {
-      color = color.color;
-      isBold = color.bold;
-      underline = color.underline;
+    if (this.level <= Logger.levels.ERROR) {
+      return this._buildAndSend(arguments, 'error');
     }
+  },
 
-    if (underline) message = message.underline;
-    message = [date, message].join(' ')[color];
-    if (isBold) message = message.bold;
+  verbose: function() {
+    return this._buildAndSend(arguments, 'verbose');
+  },
 
-    return console.log(message);
+  highlight: function() {
+    return this._buildAndSend(arguments, 'highlight');
+  },
+
+  format: function(message) {
+    return message;
+  },
+
+  setStyles: function(value) {
+    this._styles = Logger._buildStylesConfig(_.merge(value || {}, Logger.DEFAULTS.styles));
   },
 
   /**
@@ -149,11 +112,55 @@ var Logger = NOOT.Object.extend({
         level = null;
     }
 
-    if (~[null, undefined].indexOf(level)) throw new Error('Invalid logging level');
+    if (NOOT.isNone(level)) throw new Error('Invalid logging level');
 
     this.level = level;
+  },
+
+
+  _buildAndSend: function(args, styleName) {
+    var message = Logger._joinMessages(NOOT.makeArray(args));
+    message = this.format(message);
+
+    console.log(styleName);
+    var styles = this.styles[styleName];
+    if (styles) {
+      for (var style in styles) {
+        if (style === 'color') message = message[styles[style]];
+        else message = message[style];
+      }
+    }
+
+    return this.transport(message, this.onTransport);
+  },
+
+  onTransport: function() {
+
+  },
+
+  transport: function(message) {
+    console.log(message);
   }
+
 }, {
+
+  JOINER: ' ',
+
+  /**
+   * Levels enumeration
+   */
+  levels: { TRACE: 0, DEBUG: 1, INFO: 2, WARN: 3, ERROR: 4, OFF: 5 },
+
+  DEFAULTS: {
+    styles: {
+      trace: 'grey',
+      info: 'magenta',
+      warn: 'yellow',
+      error: 'red',
+      verbose: { color: 'cyan', bold: true },
+      highlight: 'inverse'
+    }
+  },
 
   /**
    * Concatenate arguments into a single valid string
@@ -167,18 +174,32 @@ var Logger = NOOT.Object.extend({
       .map(function(part) {
         switch (typeof part) {
           case 'function':
-          case 'object': return util.inspect(part, { depth: null });
+          case 'object':
+            if (part instanceof Error) return part.stack.toString();
+            return util.inspect(part, { depth: null });
           case 'undefined': return 'undefined';
           default: return part.toString();
         }
       })
-      .join(' ');
+      .join(this.JOINER);
   },
 
   /**
-   * Levels enumeration
+   *
+   *
+   * @param styles
+   * @returns {Object}
+   * @private
    */
-  levels: { TRACE: 0, DEBUG: 1, INFO: 2, WARN: 3, ERROR: 4, OFF: 5, ALWAYS: 6 }
+  _buildStylesConfig: function(styles) {
+    var ret = {};
+    for (var styleName in styles) {
+      ret[styleName] = NOOT.isNone(styles[styleName].color) ? { color: styles[styleName] } : styles[styleName];
+      ret[styleName] = _.pick(ret[styleName], ['color', 'bold', 'underline']);
+    }
+    console.log(ret);
+    return ret;
+  }
 });
 
 /**
