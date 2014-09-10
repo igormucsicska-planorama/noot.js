0196f47e8ba8162bb329e800d876a9abe0b0d352
Sylvain Estevez <estevez.sylvain@gmail.com>
2014-09-10 06:32:45 +0200

    Style changes in NOOT.Errors tests

--- test/errors/index.test.js
+++ test/errors/index.test.js
@@ -1,60 +1,46 @@
 var NOOT = nootrequire('errors');
 
 describe('NOOT.Errors', function() {
-  describe('NOOT.Errors.InternalServerError', function() {
-    it('should have right properties', function() {
-      var err = new NOOT.Errors.InternalServerError('Bad things happened');
-      err.statusCode.should.equal(500);
-      err.name.should.equal('InternalServerError');
-      err.code.should.equal('InternalServerError');
-    });
+  it('InternalServerError should have right properties', function() {
+    var err = new NOOT.Errors.InternalServerError('Bad things happened');
+    err.statusCode.should.equal(500);
+    err.name.should.equal('InternalServerError');
+    err.code.should.equal('InternalServerError');
   });
-  describe('NOOT.Errors.NotImplemented', function() {
-    it('should have right properties', function() {
-      var err = new NOOT.Errors.NotImplemented('Bad things happened');
-      err.statusCode.should.equal(501);
-      err.name.should.equal('NotImplementedError');
-      err.code.should.equal('NotImplementedError');
-    });
+  it('NotImplemented should have right properties', function() {
+    var err = new NOOT.Errors.NotImplemented('Bad things happened');
+    err.statusCode.should.equal(501);
+    err.name.should.equal('NotImplementedError');
+    err.code.should.equal('NotImplementedError');
   });
-  describe('NOOT.Errors.BadRequest', function() {
-    it('should have right properties', function() {
-      var err = new NOOT.Errors.BadRequest('Bad things happened');
-      err.statusCode.should.equal(400);
-      err.name.should.equal('BadRequestError');
-      err.code.should.equal('BadRequestError');
-    });
+  it('BadRequest should have right properties', function() {
+    var err = new NOOT.Errors.BadRequest('Bad things happened');
+    err.statusCode.should.equal(400);
+    err.name.should.equal('BadRequestError');
+    err.code.should.equal('BadRequestError');
   });
-  describe('NOOT.Errors.Unauthorized', function() {
-    it('should have right properties', function() {
-      var err = new NOOT.Errors.Unauthorized('Bad things happened');
-      err.statusCode.should.equal(401);
-      err.name.should.equal('UnauthorizedError');
-      err.code.should.equal('UnauthorizedError');
-    });
+  it('Unauthorized should have right properties', function() {
+    var err = new NOOT.Errors.Unauthorized('Bad things happened');
+    err.statusCode.should.equal(401);
+    err.name.should.equal('UnauthorizedError');
+    err.code.should.equal('UnauthorizedError');
   });
-  describe('NOOT.Errors.Forbidden', function() {
-    it('should have right properties', function() {
-      var err = new NOOT.Errors.Forbidden('Bad things happened');
-      err.statusCode.should.equal(403);
-      err.name.should.equal('ForbiddenError');
-      err.code.should.equal('ForbiddenError');
-    });
+  it('Forbidden should have right properties', function() {
+    var err = new NOOT.Errors.Forbidden('Bad things happened');
+    err.statusCode.should.equal(403);
+    err.name.should.equal('ForbiddenError');
+    err.code.should.equal('ForbiddenError');
   });
-  describe('NOOT.Errors.NotFound', function() {
-    it('should have right properties', function() {
-      var err = new NOOT.Errors.NotFound('Bad things happened');
-      err.statusCode.should.equal(404);
-      err.name.should.equal('NotFoundError');
-      err.code.should.equal('NotFoundError');
-    });
+  it('NotFound should have right properties', function() {
+    var err = new NOOT.Errors.NotFound('Bad things happened');
+    err.statusCode.should.equal(404);
+    err.name.should.equal('NotFoundError');
+    err.code.should.equal('NotFoundError');
   });
-  describe('NOOT.Errors.Conflict', function() {
-    it('should have right properties', function() {
-      var err = new NOOT.Errors.Conflict('Bad things happened');
-      err.statusCode.should.equal(409);
-      err.name.should.equal('ConflictError');
-      err.code.should.equal('ConflictError');
-    });
+  it('Conflict should have right properties', function() {
+    var err = new NOOT.Errors.Conflict('Bad things happened');
+    err.statusCode.should.equal(409);
+    err.name.should.equal('ConflictError');
+    err.code.should.equal('ConflictError');
   });
 });
