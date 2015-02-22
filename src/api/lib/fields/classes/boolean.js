554b509264373c161ffc27091256fe70cc39976d
Sylvain Estevez <estevez.sylvain@gmail.com>
2015-02-22 17:15:31 +0100

    Documented and fully tested NOOT.API.Fields.Boolean

--- src/api/lib/fields/classes/boolean.js
+++ src/api/lib/fields/classes/boolean.js
@@ -1,10 +1,47 @@
+/**
+ * Dependencies
+ */
+var NOOT = require('../../../../../')();
+
 var Field = require('../lib/field');
 
+/***********************************************************************************************************************
+ * Boolean field class.
+ *
+ * @class Boolean
+ * @namespace NOOT.API.Fields
+ * @extends NOOT.API.Field
+ * @constructor
+ **********************************************************************************************************************/
 var BooleanField = Field.extend({
+
+  /**
+   * @property supportedOperators
+   * @type Array of String
+   * @default ['eq', 'ne']
+   */
   supportedOperators: ['eq', 'ne'],
 
+  /**
+   * Turns "true" into `true` and any other value into `false`
+   *
+   * @method parseFromQueryString
+   * @param {String} value
+   * @returns {Boolean}
+   */
   parseFromQueryString: function(value) {
     return value === 'true';
+  },
+
+  /**
+   * Checks if value is a boolean. Also calls `_super()`.
+   *
+   * @method validate
+   * @param {*} value
+   * @return {Boolean}
+   */
+  validate: function(value) {
+    return this._super(value) && NOOT.isBoolean(value);
   }
 
 });
---
+++ test/api/lib/fields/classes/boolean.test.js
@@ -0,0 +1,34 @@
+var NOOT = nootrequire('api');
+
+describe('NOOT.API.Fields.Boolean', function() {
+
+  var field = NOOT.API.Fields.Boolean.create({ path: 'foo' });
+  var requiredField = NOOT.API.Fields.Boolean.create({ path: 'foo', isRequired: true });
+
+  describe('.prototype.parseFromQueryString()', function() {
+    it('should parse "true" as a true', function() {
+      field.parseFromQueryString('true').should.eql(true);
+    });
+    it('should parse any other value as false', function() {
+      field.parseFromQueryString(true).should.eql(false);
+      field.parseFromQueryString(new Date()).should.eql(false);
+      field.parseFromQueryString(123).should.eql(false);
+      field.parseFromQueryString('false').should.eql(false);
+    });
+  });
+
+  describe('.prototype.validate()', function() {
+    it('should validate a valid boolean', function() {
+      field.validate(true).should.eql(true);
+      field.validate(false).should.eql(true);
+    });
+    it('should not validate a number', function() {
+      field.validate(123).should.eql(false);
+    });
+    it('should return false (missing parameter)', function() {
+      requiredField.validate().should.eql(false);
+      requiredField.validate(null).should.eql(false);
+    });
+  });
+
+});
